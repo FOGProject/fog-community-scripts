4fdaca50cd804a041d3c7cad4c789de20d39f12d
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-02-13 22:05:45 -0600

    More tweaking

--- fogAutomatedTesting/updateNodeFOGs.sh
+++ fogAutomatedTesting/updateNodeFOGs.sh
@@ -14,7 +14,7 @@ done
 for i in "${storageNodes[@]}"
 do
     echo "Installing branch $branch onto $i"
-    printf $(ssh -t -t -o ConnectTimeout=$sshTimeout $i "cd /root/git/fogproject > /dev/null 2>&1;git reset --hard > /dev/null 2>&1;git pull > /dev/null 2>&1;git checkout $branch > /dev/null 2>&1;git pull > /dev/null 2>&1;cd bin > /dev/null 2>&1;./installfog.sh -y > /dev/null 2>&1;echo \$?") > $cwd/.$i
+    printf $(ssh -o ConnectTimeout=$sshTimeout $i "cd /root/git/fogproject > /dev/null 2>&1;git reset --hard > /dev/null 2>&1;git pull > /dev/null 2>&1;git checkout $branch > /dev/null 2>&1;git pull > /dev/null 2>&1;cd bin > /dev/null 2>&1;./installfog.sh -y > /dev/null 2>&1;echo \$?") > $cwd/.$i
 
 done
 
@@ -49,11 +49,11 @@ for i in "${storageNodes[@]}"
     if [[ "$status" == "-1" ]]; then
         complete="false"
     elif [[ "$status" == "0" ]]; then
-        echo "$i SUCCESSFULLY installed commit $(ssh -o ConnectTimeout=$sshTimeout $i "git -C /root/git/fogproject rev-parse HEAD") from branch $branch" >> $report
+        echo "$i SUCCESSFULLY installed commit $(ssh -o ConnectTimeout=$sshTimeout $i "cd /root/git/fogproject;git rev-parse HEAD") from branch $branch" >> $report
     else
 
         
-        logname=$(ssh -t -t -o ConnectTimeout=$sshTimeout $i "ls -dtr1 /root/git/fogproject/bin/error_logs/* | tail -1")
+        logname=$(ssh -o ConnectTimeout=$sshTimeout $i "ls -dtr1 /root/git/fogproject/bin/error_logs/* | tail -1")
        
         rightNow=$(date +%Y-%m-%d_%H-%M)
         mkdir -p "/var/www/html/$i/fog"
@@ -65,7 +65,7 @@ for i in "${storageNodes[@]}"
 
         scp -o ConnectTimeout=$sshTimeout $i:$logname /root/$(basename $logname)
         logname=$(basename $logname)
-        commit=$(ssh -t -t -o ConnectTimeout=$sshTimeout $i "cd /root/git/fogproject;git rev-parse HEAD")
+        commit=$(ssh -o ConnectTimeout=$sshTimeout $i "cd /root/git/fogproject;git rev-parse HEAD")
 
         echo "Date=$rightNow" > /var/www/html/$i/fog/${rightNow}.log
         echo "Branch=$branch" >> /var/www/html/$i/fog/${rightNow}.log
