a13130011e2cea5ad047efeb11e81cd09c212eaa
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-02-16 19:17:40 -0600

    Tweaks to new script

--- fogAutomatedTesting/installBranch.sh
+++ fogAutomatedTesting/installBranch.sh
@@ -12,18 +12,6 @@ branch=$1
 #  Error 5 = Failed to change directory.
 #  Error 6 = Installation failed.
 #  All else "success".
-error() {
-    local errCode=$1
-        case $errCode in
-            2) echo "Failed to reset git" ;;
-            3) echo "Failed to pull git" ;;
-            4) echo "Failed to checkout git" ;;
-            5) echo "Failed to change directory" ;;
-            6) echo "Installation failed" ;;
-            *) exit 0 ;;
-        esac
-        exit $errCode
-}
 usage() {
     local errCode=$1
     echo "Usage $0 <branch>"
@@ -33,23 +21,23 @@ usage() {
 export PATH="$PATH:/usr/bin/core_perl"
 cd /root/git/fogproject >/dev/null 2>&1
 stat=$?
-[[ ! $stat -eq 0 ]] && error 5
+[[ ! $stat -eq 0 ]] && exit 5
 git reset --hard >/dev/null 2>&1
 stat=$?
-[[ ! $stat -eq 0 ]] && error 2
+[[ ! $stat -eq 0 ]] && exit 2
 git pull >/dev/null 2>&1
 stat=$?
-[[ ! $stat -eq 0 ]] && error 3
+[[ ! $stat -eq 0 ]] && exit 3
 git checkout $branch >/dev/null 2>&1
 stat=$?
-[[ ! $stat -eq 0 ]] && error 4
+[[ ! $stat -eq 0 ]] && exit 4
 git pull >/dev/null 2>&1
 stat=$?
-[[ ! $stat -eq 0 ]] && error 2
+[[ ! $stat -eq 0 ]] && exit 2
 cd bin >/dev/null 2>&1
 stat=$?
-[[ ! $stat -eq 0 ]] && error 5
+[[ ! $stat -eq 0 ]] && exit 5
 ./installfog -y >/dev/null 2>&1
 stat=$?
-[[ ! $stat -eq 0 ]] && error 6
+[[ ! $stat -eq 0 ]] && exit 6
 error 0
