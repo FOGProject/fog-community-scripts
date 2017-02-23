1519c26e8684e3a5926dd490db53982a70ac7cb1
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-02-23 08:48:02 -0600

    Make sure VMs get turned off.

--- fogAutomatedTesting/shutdownVMs.sh
+++ fogAutomatedTesting/shutdownVMs.sh
@@ -8,4 +8,9 @@ for i in "${storageNodes[@]}"
 do
     ssh -o ConnectTimeout=$sshTimeout $hostsystem "virsh shutdown $i > /dev/null 2>&1"
 done
-
+sleep 120
+#Start the commands going in unison.
+for i in "${storageNodes[@]}"
+do
+    ssh -o ConnectTimeout=$sshTimeout $hostsystem "virsh destroy $i > /dev/null 2>&1"
+done
