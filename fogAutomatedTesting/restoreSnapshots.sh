c7e85791d7d2f0a103dc703b05860d4677db2753
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-04-27 08:43:26 -0500

    Add timeout and tire kick calls to VM restoring.

--- fogAutomatedTesting/restoreSnapshots.sh
+++ fogAutomatedTesting/restoreSnapshots.sh
@@ -5,10 +5,11 @@ source "$cwd/settings.sh"
 snapshotName=$1
 
 
-
-#Start the commands going in unison.
 for i in "${storageNodes[@]}"
 do
+    nonsense=$(timeout $sshTime ssh -o ConnectTimeout=$sshTimeout $hostsystem "echo wakeup")
+    nonsense=$(timeout $sshTime ssh -o ConnectTimeout=$sshTimeout $hostsystem "echo get ready")
+    sleep 5
     echo "$(date +%x_%r) Restoring snapshot $snapshotName to $i" >> $output
     ssh -o ConnectTimeout=$sshTimeout $hostsystem "virsh snapshot-revert $i $snapshotName > /dev/null 2>&1"
 done
