96a24da081f1219353ea32d799b6253c32055b3b
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-04-27 08:46:30 -0500

    Add timeouts and kicks and time to reboots.

--- fogAutomatedTesting/rebootVMs.sh
+++ fogAutomatedTesting/rebootVMs.sh
@@ -2,11 +2,15 @@
 cwd="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$cwd/settings.sh"
 
+nonsense=$(timeout $sshTime ssh -o ConnectTimeout=$sshTimeout $hostsystem "echo wakeup")
+nonsense=$(timeout $sshTime ssh -o ConnectTimeout=$sshTimeout $hostsystem "echo get ready")
+sleep 5
 
 #Gracefully shutdown all VMs.
 for i in "${storageNodes[@]}"
 do
     ssh -o ConnectTimeout=$sshTimeout $hostsystem "virsh shutdown $i > /dev/null 2>&1"
+    sleep 5
 done
 sleep 30
 
@@ -15,8 +19,9 @@ sleep 30
 for i in "${storageNodes[@]}"
 do
     ssh -o ConnectTimeout=$sshTimeout $hostsystem "virsh destroy $i > /dev/null 2>&1"
+    sleep 5
 done
-sleep 10
+sleep 5
 
 
 
@@ -24,6 +29,7 @@ sleep 10
 for i in "${storageNodes[@]}"
 do
     ssh -o ConnectTimeout=$sshTimeout $hostsystem "virsh start $i > /dev/null 2>&1"
+    sleep 5
 done
 howLongToWait=120
 sleep $howLongToWait
