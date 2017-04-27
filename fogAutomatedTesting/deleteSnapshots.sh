c67158d01da8925867e149ef550c091604b14fea
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-04-27 08:44:30 -0500

    Add timeout and tire kick to snapshot deleting.

--- fogAutomatedTesting/deleteSnapshots.sh
+++ fogAutomatedTesting/deleteSnapshots.sh
@@ -8,6 +8,9 @@ snapshotName=$1
 #Start the commands going in unison.
 for i in "${storageNodes[@]}"
 do
+    nonsense=$(timeout $sshTime ssh -o ConnectTimeout=$sshTimeout $hostsystem "echo wakeup")
+    nonsense=$(timeout $sshTime ssh -o ConnectTimeout=$sshTimeout $hostsystem "echo get ready")
+    sleep 5
     echo "$(date +%x_%r) Deleting snapshot $snapshotName from $i" >> $output
     ssh -o ConnectTimeout=$sshTimeout $hostsystem "virsh snapshot-delete $i $snapshotName > /dev/null 2>&1"
 
