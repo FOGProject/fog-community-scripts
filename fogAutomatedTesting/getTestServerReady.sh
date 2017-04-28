c29cc1a6c7e2cf970b85362adc62be5bed1755a9
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-04-27 21:49:55 -0500

    Removed unnecessary message.

--- fogAutomatedTesting/getTestServerReady.sh
+++ fogAutomatedTesting/getTestServerReady.sh
@@ -7,9 +7,6 @@ source "$cwd/settings.sh"
 branch="working"
 
 
-echo "$(date +%x_%r) Configuring the test FOG Server with \"$branch\"" >> $output
-echo "Configuring the test FOG Server with \"$branch\"" >> $report
-
 #Start the server up.
 echo "$(date +%x_%r) Starting up \"$testServerVMName\"" >> $output
 ssh -o ConnectTimeout=$sshTimeout $hostsystem "virsh start $testServerVMName > /dev/null 2>&1"
@@ -31,18 +28,18 @@ printf $(timeout $fogTimeout ssh -o ConnectTimeout=$sshTimeout $testServerSshAli
 timeout $sshTime ssh -o ConnectTimeout=$sshTimeout $testServerSshAlias "rm -f /root/installBranch.sh"
 status=$(cat $cwd/.$testServerSshAlias)
 
-echo "$(date +%x_%r) Return code was \"$status\"" >> $output
-echo "Return code was \"$status\"" >> $report
-
 #Cleanup after all is done.
 rm -f $cwd/.$testServerSshAlias
 
 
 if [[ "$status" != "0" ]]; then
-    echo "$(date +%x_%r) Non-zero exit code, not continuing." >> $output
-    echo "Non-zero exit code, not continuing." >> $report
+    echo "$(date +%x_%r) Exit code was \"$status\", not continuing." >> $output
+    echo "Exit code was \"$status\", not continuing." >> $report
     exit $status
 fi
+
+echo "$(date +%x_%r) Update was successful." >> $output
+echo "Update was successful." >> $report
 
 exit 0
 
