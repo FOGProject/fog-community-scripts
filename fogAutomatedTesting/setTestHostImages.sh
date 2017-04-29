7c974de7ce28a0bb76a666a9d8913f4db15865e4
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-04-29 11:44:09 -0500

    Remove troubleshooting.

--- fogAutomatedTesting/setTestHostImages.sh
+++ fogAutomatedTesting/setTestHostImages.sh
@@ -25,11 +25,5 @@ fi
 echo "$(date +%x_%r) Setting FOG hosts \"$hostIDs\" image IDs to $imageID" >> $output
 
 #Set all the fog hosts to the same image.
-cmd="curl -k -H 'Content-Type: application/json' -H 'fog-user-token: ${testServerUserToken}' -H 'fog-api-token: ${testServerApiToken}' http://${testServerIP}/fog/image/${imageID}/edit -X PUT -d '{\"hosts\": [${hostIDs}]}' -vvv"
-echo -e "Command to be run:\n\t${cmd}"
-echo
-echo
-echo here
-echo
-echo "Running command now"
+cmd="curl -k -H 'Content-Type: application/json' -H 'fog-user-token: ${testServerUserToken}' -H 'fog-api-token: ${testServerApiToken}' http://${testServerIP}/fog/image/${imageID}/edit -X PUT -d '{\"hosts\": [${hostIDs}]}'"
 eval $cmd
