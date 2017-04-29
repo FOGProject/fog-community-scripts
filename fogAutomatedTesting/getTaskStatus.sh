066d6564a9426325026334c088b0eba08bd6b769
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-04-29 14:13:38 -0500

    Use jq to parse count.

--- fogAutomatedTesting/getTaskStatus.sh
+++ fogAutomatedTesting/getTaskStatus.sh
@@ -4,6 +4,11 @@ cwd="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$cwd/settings.sh"
 
 
+if [[ -z $(command -v jq) ]]; then
+    echo "This requires jq to be installed, exiting."
+    exit 1
+fi
+
 
 #Ask for the host ID to be passed in.
 if [[ -z $1 ]]; then
@@ -16,5 +21,7 @@ fi
 
 cmd="curl --silent -X GET -H 'content-type: application/json' -H 'fog-user-token: ${testServerUserToken}' -H 'fog-api-token: ${testServerApiToken}' http://${testServerIP}/fog/task/active -d '{\"hostID\": [${hostID}]}'"
 result=$(eval $cmd)
-echo $result
+
+echo $result | jq '.count'
+
 
