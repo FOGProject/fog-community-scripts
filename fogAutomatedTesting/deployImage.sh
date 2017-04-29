3fa669340bba35a7dc34f45c7931923ec9e04170
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-04-29 14:28:47 -0500

    Add missing files.

---
+++ fogAutomatedTesting/deployImage.sh
@@ -0,0 +1,8 @@
+
+
+
+
+
+curl -ku "fog:password" --header "content-type: application/json" --header "fog-api-token: MzI2NDY1NjY2NjM0MzUzMDMzMzA2MzM1MzEzNzYyMzg2NTYyNjQ2MjMxMzczMTM0NjY2NDM0NjUzOTM2NjIzNDM4MzQ2NDM3MzY2MzM2MzMzNjYyMzUzODY0MzUzNDYyMzgzMDY2NjQzNTMxMzI2MzM5NjYzNjYzMzMzMzM0MzA=" http://10.0.0.28/fog/image/1/edit -X PUT -d '{"hosts": [1,2,3]}'
+
+
---
+++ fogAutomatedTesting/shutdownTestServer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cwd="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$cwd/settings.sh"
+
+echo "$(date +%x_%r) Shutting down $testServerVMName" >> $output
+ssh -o ConnectTimeout=$sshTimeout $hostsystem "virsh shutdown $testServerVMName > /dev/null 2>&1"
+sleep 60
+ssh -o ConnectTimeout=$sshTimeout $hostsystem "virsh destroy $testServerVMName > /dev/null 2>&1"
+
+
+
