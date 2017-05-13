b7bb80cbeaa8bc9eb866b3d7e27fa69bfdaabd46
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-05-13 09:57:53 -0500

    Fix canceling all tasks

--- fogAutomatedTesting/cancelTasks.sh
+++ fogAutomatedTesting/cancelTasks.sh
@@ -9,9 +9,9 @@ if [[ -z $(command -v jq) ]]; then
     exit 1
 fi
 
-#Get the host ID if present. Having just a blank variable within brackets breaks it.
+#Get the host ID if present. If present, just delete host's tasks. Else, delete all tasks.
 if [[ -z $1 ]]; then
-    cmd="timeout $sshTimeout curl --silent -X GET -H 'content-type: application/json' -H 'fog-user-token: ${testServerUserToken}' -H 'fog-api-token: ${testServerApiToken}' http://${testServerIP}/fog/task/cancel -d '{\"hostID\": }'"
+    cmd="timeout $sshTimeout curl --silent -X DELETE -H 'content-type: application/json' -H 'fog-user-token: ${testServerUserToken}' -H 'fog-api-token: ${testServerApiToken}' http://${testServerIP}/fog/task/cancel -d '{\"typeID\": [1,2] }'"
 else
     hostID=$1
     cmd="timeout $sshTimeout curl --silent -X GET -H 'content-type: application/json' -H 'fog-user-token: ${testServerUserToken}' -H 'fog-api-token: ${testServerApiToken}' http://${testServerIP}/fog/task/cancel -d '{\"hostID\": [${hostID}]}'"
@@ -20,5 +20,4 @@ fi
 #Run the command.
 result=$(eval $cmd)
 
-echo $cmd
 
