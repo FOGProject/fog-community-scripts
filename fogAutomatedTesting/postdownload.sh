14d80a4d0f10c2b6bc0fcbd3d330af0a4b94fc7a
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-05-16 08:28:22 -0500

    Try to tar the logs.

--- fogAutomatedTesting/postdownload.sh
+++ fogAutomatedTesting/postdownload.sh
@@ -1,50 +1,10 @@
 #!/bin/bash
 
-
-#The below are from sourcing the funcs.sh script.
-# ftp		# IP Address from Storage Management-General
-# hostname	# Host Name from Host Managment-General
-# img		# Image Name from Image Management-General
-# mac		# Primary MAC from Image Management-General
-# osid		# Host Image Index number from Image Management-General
-# storage		# IP Address + Image Path from Storage Management-General
-# storageip	# IP Address from Storage Management-General
-# web		#IP Address + Web root from Storage Management-General
-
-#The below are from querying the fog server.
-# shutdown		# Shut down at the end of imaging
-# hostdesc		#Host Description from Host Managment-General
-# hostip		# IP address of the FOS client
-# hostimageid		# ID of image being deployed
-# hostbuilding		# Left over from very old versions of fog, not used anymore.
-# hostusead		# Join Domain after image task from Host Management-Active Directory
-# hostaddomain		# Domain name from Host Management-Active Directory
-# hostaduser		# Domain Username from Host Management-Active Directory
-# hostadou		# Organizational Unit from Host Management-Active Directory
-# hostproductkey=	# Host Product Key from Host Management-Active Directory
-# imagename		# Image Name from Image Management-General
-# imagedesc		# Image Description from Image Management-General
-# imageosid		# Operating System from Image Management-General
-# imagepath		# Image Path from Image Management-General (/images/ assumed)
-# primaryuser		# Primary User from Host Management-Inventory
-# othertag		# Other Tag #1 User from Host Management-Inventory
-# othertag1		# Other Tag #2 from Host Management-Inventory
-# sysman		# System Manufacturer from Host Management-Inventory (from SMBIOS)
-# sysproduct		# System Product from Host Management-Inventory (from SMBIOS)
-# sysserial		# System Serial Number from Host Management-Inventory (from SMBIOS)
-# mbman			# Motherboard Manufacturer from Host Management-Inventory (from SMBIOS)
-# mbserial		# Motherboard Serial Number from Host Management-Inventory (from SMBIOS)
-# mbasset		# Motherboard Asset Tag from Host Management-Inventory (from SMBIOS)
-# mbproductname		# Motherboard Product Name from Host Management-Inventory (from SMBIOS)
-# caseman		# Chassis Manufacturer from Host Management-Inventory (from SMBIOS)
-# caseserial		# Chassis Serial from Host Management-Inventory (from SMBIOS)
-# caseasset		# Chassis Asset from Host Management-Inventory (from SMBIOS)
-# location		# Host Location (name) from Host Management-General
-
-
 #Formulate filename.
-postDownloadOutput="/var/log/postdownload.log"
+postDownloadOutput="/fogtesting/postdownload.log"
+touch $postDownloadOutput
 
+tar -zcvf postdownloadlogs.tar.gz /tmp/ -C /fogtesting
 
 echo "#################### sfdisk -Fl /dev/sda" > $postDownloadOutput
 sfdisk -Fl /dev/sda >> $postDownloadOutput
@@ -78,4 +38,5 @@ echo "#################### df -h" >> $postDownloadOutput
 df -h >> $postDownloadOutput
 echo "" >> $postDownloadOutput
 echo "" >> $postDownloadOutput
+
 
--- fogAutomatedTesting/postinit.sh
+++ fogAutomatedTesting/postinit.sh
@@ -3,68 +3,17 @@
 #Sourcing the functions file gives us access to all kernel variables and many neat functions.
 source /usr/share/fog/lib/funcs.sh
 
-#Getting this file from the server and then sourcing it gives us access to a wealth of variables about this host.
-if [[ ! -z $mac ]]; then
-    wget -q -O /tmp/hinfo.txt "http://${web}service/hostinfo.php?mac=$mac"
-    if [[ -f /tmp/hinfo.txt ]]; then
-        . /tmp/hinfo.txt
-    fi
-fi
+#Mount share to /fogtesting
+
+mkdir /fogtesting
+mount -t cifs //10.0.0.25/fogtesting/$hostname /fogtesting -o username=fogtesting -o password=testing
 
 
-#The below are from sourcing the funcs.sh script.
-# ftp		# IP Address from Storage Management-General
-# hostname	# Host Name from Host Managment-General
-# img		# Image Name from Image Management-General
-# mac		# Primary MAC from Image Management-General
-# osid		# Host Image Index number from Image Management-General
-# storage		# IP Address + Image Path from Storage Management-General
-# storageip	# IP Address from Storage Management-General
-# web		#IP Address + Web root from Storage Management-General
 
-#The below are from querying the fog server.
-# shutdown		# Shut down at the end of imaging
-# hostdesc		#Host Description from Host Managment-General
-# hostip		# IP address of the FOS client
-# hostimageid		# ID of image being deployed
-# hostbuilding		# Left over from very old versions of fog, not used anymore.
-# hostusead		# Join Domain after image task from Host Management-Active Directory
-# hostaddomain		# Domain name from Host Management-Active Directory
-# hostaduser		# Domain Username from Host Management-Active Directory
-# hostadou		# Organizational Unit from Host Management-Active Directory
-# hostproductkey=	# Host Product Key from Host Management-Active Directory
-# imagename		# Image Name from Image Management-General
-# imagedesc		# Image Description from Image Management-General
-# imageosid		# Operating System from Image Management-General
-# imagepath		# Image Path from Image Management-General (/images/ assumed)
-# primaryuser		# Primary User from Host Management-Inventory
-# othertag		# Other Tag #1 User from Host Management-Inventory
-# othertag1		# Other Tag #2 from Host Management-Inventory
-# sysman		# System Manufacturer from Host Management-Inventory (from SMBIOS)
-# sysproduct		# System Product from Host Management-Inventory (from SMBIOS)
-# sysserial		# System Serial Number from Host Management-Inventory (from SMBIOS)
-# mbman			# Motherboard Manufacturer from Host Management-Inventory (from SMBIOS)
-# mbserial		# Motherboard Serial Number from Host Management-Inventory (from SMBIOS)
-# mbasset		# Motherboard Asset Tag from Host Management-Inventory (from SMBIOS)
-# mbproductname		# Motherboard Product Name from Host Management-Inventory (from SMBIOS)
-# caseman		# Chassis Manufacturer from Host Management-Inventory (from SMBIOS)
-# caseserial		# Chassis Serial from Host Management-Inventory (from SMBIOS)
-# caseasset		# Chassis Asset from Host Management-Inventory (from SMBIOS)
-# location		# Host Location (name) from Host Management-General
+postInitOutput="/fogtesting/postinit.log"
+touch $postInitOutput
 
-
-#Map to a windows/cifs share using username and password.
-#We mount to /var/log so we can caputre all logs in FOS.
-sleep 15
-mv /var/log /var/log.old
-mkdir -p /var/log
-mount -t cifs //10.0.0.25/fogtesting/${hostname} /var/log -o username=fogtesting,password=testing
-mv /var/log.old/* /var/log
-rm -rf /var/log.old
-sleep 15
-
-#Formulate filename.
-postInitOutput="/var/log/postinit.log"
+tar -zcvf postinitlogs.tar.gz /tmp/ -C /fogtesting
 
 
 echo "#################### sfdisk -Fl /dev/sda" > $postInitOutput
@@ -99,7 +48,4 @@ echo "#################### df -h" >> $postInitOutput
 df -h >> $postInitOutput
 echo "" >> $postInitOutput
 echo "" >> $postInitOutput
-
-
-
 
