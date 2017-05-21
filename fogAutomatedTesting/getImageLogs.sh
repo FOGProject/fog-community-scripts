f9bb52f0b2406b2e1c376024a11ae9c8196475e4
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-05-21 10:18:19 -0500

    Fix variable and spelling

--- fogAutomatedTesting/getImageLogs.sh
+++ fogAutomatedTesting/getImageLogs.sh
@@ -5,48 +5,50 @@ source "$cwd/settings.sh"
 
 rightNow=$(date +%Y-%m-%d_%H-%M)
 
+testHost=$1
 
-if [[ -f ${shareDir}/${1}/var-log.tar ]]; then
-    mv ${shareDir}/${1}/var-log.tar ${webdir}/${1}/${rightNow}_var-log.tar
-    echo "$(date +%x_%r) \"$1\" /var/log here: ${domanName}/${1}/${rightNow}_var-log.tar" >> $output
-    echo "\"$1\" /var/log here: ${domanName}/${1}/${rightNow}_var-log.tar" >> $report
+
+if [[ -f ${shareDir}/${testHost}/var-log.tar ]]; then
+    mv ${shareDir}/${testHost}/var-log.tar ${webdir}/${testHost}/${rightNow}_var-log.tar
+    echo "$(date +%x_%r) \"$testHost\" /var/log here: ${domainName}/${testHost}/${rightNow}_var-log.tar" >> $output
+    echo "\"$testHost\" /var/log here: ${domainName}/${testHost}/${rightNow}_var-log.tar" >> $report
 else
-    echo "$(date +%x_%r) \"$1\" /var/log could not be retrieved." >> $output
-    echo "\"$1\" /var/log could not be retrieved." >> $report
+    echo "$(date +%x_%r) \"$testHost\" /var/log could not be retrieved." >> $output
+    echo "\"$testHost\" /var/log could not be retrieved." >> $report
 fi
 
-if [[ -f ${shareDir}/${1}/postinit.log ]]; then
-    mv ${shareDir}/${1}/postinit.log ${webdir}/${1}/${rightNow}_postinit.log
-    echo "$(date +%x_%r) \"$1\" postinit.log: ${domanName}/${1}/${rightNow}_postinit.log" >> $output
-    echo "\"$1\" postinit.log: ${domanName}/${1}/${rightNow}_postinit.log" >> $report
+if [[ -f ${shareDir}/${testHost}/postinit.log ]]; then
+    mv ${shareDir}/${testHost}/postinit.log ${webdir}/${testHost}/${rightNow}_postinit.log
+    echo "$(date +%x_%r) \"$testHost\" postinit.log: ${domainName}/${testHost}/${rightNow}_postinit.log" >> $output
+    echo "\"$testHost\" postinit.log: ${domainName}/${testHost}/${rightNow}_postinit.log" >> $report
 else
-    echo "$(date +%x_%r) \"$1\" postinit.log could not be retrieved." >> $output
-    echo "\"$1\" postinit.log could not be retrieved." >> $report
+    echo "$(date +%x_%r) \"$testHost\" postinit.log could not be retrieved." >> $output
+    echo "\"$testHost\" postinit.log could not be retrieved." >> $report
 fi
 
-if [[ -f ${shareDir}/${$1}/postdownload.log ]]; then
-    mv ${shareDir}/${$1}/postdownload.log ${webdir}/${$1}/${rightNow}_postdownload.log
-    echo "$(date +%x_%r) \"$1\" postdownload.log: ${domanName}/${1}/${rightNow}_postdownload.log" >> $output
-    echo "\"$1\" postdownload.log: ${domanName}/${1}/${rightNow}_postdownload.log" >> $report
+if [[ -f ${shareDir}/${$testHost}/postdownload.log ]]; then
+    mv ${shareDir}/${$testHost}/postdownload.log ${webdir}/${$testHost}/${rightNow}_postdownload.log
+    echo "$(date +%x_%r) \"$testHost\" postdownload.log: ${domainName}/${testHost}/${rightNow}_postdownload.log" >> $output
+    echo "\"$testHost\" postdownload.log: ${domainName}/${testHost}/${rightNow}_postdownload.log" >> $report
 else
-    echo "$(date +%x_%r) \"$1\" postdownload.log could not be retrieved." >> $output
-    echo "\"$1\" postdownload.log could not be retrieved." >> $report
+    echo "$(date +%x_%r) \"$testHost\" postdownload.log could not be retrieved." >> $output
+    echo "\"$testHost\" postdownload.log could not be retrieved." >> $report
 fi
 
 #Screenshots.
-count=$(ls -1 ${shareDir}/${1}/*.ppm 2>/dev/null | wc -l)
+count=$(ls -1 ${shareDir}/${testHost}/*.ppm 2>/dev/null | wc -l)
 if [[ $count -gt 0 ]]; then
-    tar -cf ${webdir}/${1}/${rightNow}_deploy_screenshots.tar -C ${shareDir}/${1}/*.ppm .
-    echo "$(date +%x_%r) \"$1\" deploy screenshots: ${domanName}/${1}/${rightNow}_deploy_screenshots.tar" >> $output
-    echo "\"$1\" deploy screenshots: ${domanName}/${1}/${rightNow}_deploy_screenshots.tar" >> $report
+    tar -cf ${webdir}/${testHost}/${rightNow}_deploy_screenshots.tar -C ${shareDir}/${testHost}/*.ppm .
+    echo "$(date +%x_%r) \"$testHost\" deploy screenshots: ${domainName}/${testHost}/${rightNow}_deploy_screenshots.tar" >> $output
+    echo "\"$testHost\" deploy screenshots: ${domainName}/${testHost}/${rightNow}_deploy_screenshots.tar" >> $report
 else
-    echo "$(date +%x_%r) \"$1\" No deploy screenshots could be retrieved." >> $output
-    echo "\"$1\" No deploy screenshots could be retrieved." >> $report
+    echo "$(date +%x_%r) \"$testHost\" No deploy screenshots could be retrieved." >> $output
+    echo "\"$testHost\" No deploy screenshots could be retrieved." >> $report
 
 fi
 
 #Cleanup.
-rm -rf ${shareDir}/${1}
+rm -rf ${shareDir}/${testHost}
 
 
 
