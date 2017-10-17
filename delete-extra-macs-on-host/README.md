3b04a64fdfb7b58343233e50e257a282a375e70b
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2017-10-17 07:05:32 -0500

    Works

--- delete-extra-macs-on-host/README.md
+++ delete-extra-macs-on-host/README.md
@@ -3,10 +3,8 @@
 ---
 
 This is a simple BASH script that deletes all extra MACs associated with a particular host.
-In the script at the top, set the hostID and MAC that you want to keep. 
+In the script at the top, set the hostID and MAC that you want to keep associated with that hostID. 
 All other MACs associated with that hostID will be deleted.
-
-
 
 Create a root crontab event. You will need to be root to do this.
 You can switch to root with:
@@ -16,12 +14,14 @@ Then enter into root's crontab with:
 crontab -e
 
 
-Add this line to the file to run the script every minute. The path should be the path to this script.
+Add this line to the file to run the script every minute. The path should be the path to the script.
 * * * * * /root/git/fog-community-scripts/delete-extra-macs-on-host/delete-extra-macs.sh
 
 Or this to run every 3 minutes:
 */3 * * * * /root/git/fog-community-scripts/delete-extra-macs-on-host/delete-extra-macs.sh
 
+Or once a day:
+0 12 * * * /root/git/fog-community-scripts/delete-extra-macs-on-host/delete-extra-macs.sh
 
 This tool is licensed under the fog community scripts license.
 
--- delete-extra-macs-on-host/delete-extra-macs.sh
+++ delete-extra-macs-on-host/delete-extra-macs.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-hostID = "0" # Put the host's ID here.
-macToKeep = "aa:bb:cc:dd:ee:ff" # Put the mac you want to keep here.
+#----- Settings -----#
+hostID="0" # Put the host's ID here.
+macToKeep="aa:bb:cc:dd:ee:ff" # Put the mac you want to keep here.
 
 
-#-----Get command paths-----#
+
+
+#----- Get command paths -----#
 mysql=$(command -v mysql)
+
+
 
 #----- MySQL Credentials -----#
 source /opt/fog/.fogsettings
@@ -13,26 +18,21 @@ source /opt/fog/.fogsettings
 
 
 #----- Queries ------#
-deleteExtraMacs="SELECT \`ngmHostname\`, \`ngmMemberName\` FROM \`nfsGroupMembers\`"
+deleteExtraMacs="DELETE FROM hostMAC WHERE hmHostID = '${hostID}' AND hmMAC != '${macToKeep}'"
 
 
 
 #----- Build mysql options -----#
 options="-sN"
-if [[$snmysqlhost != ""]]; then
-        options="$options -h$snmysqlhost"
-fi
-if [[$snmysqluser != ""]]; then
-        options="$options -u$snmysqluser"
-fi
-if [[$snmysqlpass != ""]]; then
-        options="$options -p$snmysqlpass"
-fi
+[[ $snmysqlhost != "" ]] && options="$options -h${snmysqlhost}"
+[[ $snmysqluser != "" ]] && options="$options -u${snmysqluser}"
+[[ $snmysqlpass != "" ]] && options="$options -p${snmysqlpass}"
 options="$options -D fog -e"
 
 
 
-#Execute the command.
-$mysql $options "$deleteExtraMacs"
+#----- Execute the command -----#
+$mysql $options "${deleteExtraMacs}"
 
-
+# Troubleshooting line, echos the command.
+# echo "$mysql $options \"${deleteExtraMacs}\""
