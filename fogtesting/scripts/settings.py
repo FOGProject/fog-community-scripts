635176bd490c04bc6216af8c1068707e0dccda13
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2018-11-24 19:54:39 +0000

    Add comments

--- fogtesting/scripts/settings.py
+++ fogtesting/scripts/settings.py
@@ -1,10 +1,14 @@
-
+# The list of OSs.
 OSs = ["debian9"]
 
+# The list of branches to process.
 branches = ["master"]
 
+# The region we operate in, dictated by terraform.
 theRegion = "us-east-2"
+# The availibility zone, which we use just one zone.
 zone = theRegion + 'a'
 
+# For when we need to wait for something to get done while in a loop, wait this long.
 wait = 1
 
