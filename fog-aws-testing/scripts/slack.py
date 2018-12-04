51f352840fcc121de61a06e8e82112bf8b26b04f
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2018-12-03 22:05:36 -0600

    Added slack example

---
+++ fog-aws-testing/scripts/slack.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import slackweb
+
+# This reads an environment variable named WEBHOOK_URL
+slack = slackweb.Slack(os.environ['WEBHOOK_URL'])
+
+# This is how to send a message.
+slack.notify(text="We have a winner", channel="distro_check", username="distro_checker", icon_emoji=":ghost:")
+
+
