73e10b18b5b61227c2b57738b8f10b626d71498b
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2018-12-04 02:17:42 +0000

    Added troubleshooting

--- fog-aws-testing/scripts/test_all.py
+++ fog-aws-testing/scripts/test_all.py
@@ -9,43 +9,53 @@ def runTest(branch,os,webdir,statusDir,now):
     make_dir(statusDir)
     
     # Create hidden file for node - for status reporting.
-    newDashboard = os.path.join(webdir,"index.html")
+    print "Creating " + os.path.join(statusDir,name + "." + branch)
     with open(os.path.join(statusDir,name + "." + branch), 'w') as content_file:
         content_file.write("-1") 
 
+    print  "Kickin tires"
     # Kick the tires a bit, this helps the remote host to 'wake up', and for a network path to be learned by involved routers.
     subprocess.call(timeout + " " + sshTime + " " + ssh + " -o ConnectTimeout=" + sshTimeout + " " + os + ' "echo wakeup"', shell=True)
     subprocess.call(timeout + " " + sshTime + " " + ssh + " -o ConnectTimeout=" + sshTimeout + " " + os + ' "echo get ready"', shell=True)
 
+    print "Scp script to remote box"
     # Scp a script onto the remote box that we will later call.
     subprocess.call(timeout + " " + sshTime + " " + scp + " -o ConnectTimeout=" + sshTimeout + " " + os.path.join(cwd,'installBranch.sh') + " " + os + ":/root/installBranch.sh", shell=True)
 
+    print "Starting installer"
     # Start the fog installer.
     subprocess.call(timeout + " " + fogTimeout + " " + ssh + " -o ConnectTimeout=" + sshTimeout + " " + os + ' "/root/./installBranch.sh ' + branch + '"', shell=True)
 
+    print "Getting result file"
     # Get the result file.
     subprocess.call(timeout + " " + sshTime + " " + scp + " -o ConnectTimeout=" + sshTimeout + " " + os + ":/root/result " + os.path.join(statusDir,os + "." + branch + ".result"), shell=True)
     # This should send the result code of the attempt to something like /tmp/debian9.master.result
 
+    print "Getting fog log file"
     # Get the fog log.
     subprocess.call(timeout + " " + sshTime + " " + scp + " -o ConnectTimeout=" + sshTimeout + " " + os + ":/root/git/fogproject/bin/error_logs/fog_error* " + os.path.join(webdir,os,now + "_installer.log"), shell=True)
 
+    print "Getting apache logs"
     # Get the apache error logs. Can be in only two places.
     subprocess.call(timeout + " " + sshTime + " " + scp + " -o ConnectTimeout=" + sshTimeout + " " + os + ":/var/log/httpd/error_log " + os.path.join(webdir,os,now + "_apache.log"), shell=True)
     subprocess.call(timeout + " " + sshTime + " " + scp + " -o ConnectTimeout=" + sshTimeout + " " + os + ":/var/log/apache2/error.log " + os.path.join(webdir,os,now + "_apache.log"), shell=True)
 
+    print "Getting php-fpm logs"
     # Get php-fpm logs. Can be in only two places.
     subprocess.call(timeout + " " + sshTime + " " + scp + " -o ConnectTimeout=" + sshTimeout + " " + os + ":/var/log/php-fpm/www-error.log " + os.path.join(webdir,os,now + "_php-fpm.log"), shell=True)
     subprocess.call(timeout + " " + sshTime + " " + scp + " -o ConnectTimeout=" + sshTimeout + " " + os + ":/var/log/php-fpm/error.log " + os.path.join(webdir,os,now + "_php-fpm.log"), shell=True)
 
+    print "Getting commit"
     # Get the commit the remote node was using, just as a sainity check.
     subprocess.call(timeout + " " + fogTimeout + " " + ssh + " -o ConnectTimeout=" + sshTimeout + " " + os + 'cd /root/git/fogproject;git rev-parse HEAD > /root/commit', shell=True)
     subprocess.call(timeout + " " + sshTime + " " + scp + " -o ConnectTimeout=" + sshTimeout + " " + os + ":/root/commit " + os.path.join(statusDir,os + "." + branch + ".commit"), shell=True)
     # This should send just the commit that was used in the test to something like /tmp/debian9.master.commit
 
+    print "Reading commit"
     # Read the commit.
     commit = read_file(os.path.join(statusDir,os + "." + branch + ".commit"))
 
+    print "Rebuilding log"
     # Rebuild the log file to have information at the top of it.
     log = "Date=" + now + "\n"
     log = log + "Branch=" + branch + "\n"
@@ -54,6 +64,7 @@ def runTest(branch,os,webdir,statusDir,now):
     log = log + "##### Begin Log #####\n"
     log = log + read_file(os.path.join(webdir,os,now + "_installer.log"))
 
+    print "Writing log"
     # Write the new log.
     with open(os.path.join(webdir,os,now + "_installer.log"), 'w') as content_file:
         content_file.write(log)
