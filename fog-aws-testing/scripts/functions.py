8889b2c5f65d128323f2f19b635f6c41b58a3534
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2018-12-08 18:01:58 +0000

    Move giant function to functions, increase sleep for instance startup, try more times to 'wake up' instance when getting identity

--- fog-aws-testing/scripts/functions.py
+++ fog-aws-testing/scripts/functions.py
@@ -1,5 +1,7 @@
 import boto3
 import time
+import sys
+import datetime
 from settings import *
 import os 
 from threading import Thread
@@ -12,11 +14,115 @@ cwd = os.path.dirname(os.path.realpath(__file__))
 ec2resource = boto3.resource('ec2')
 ec2client = boto3.client('ec2')
 
+
+def runTest(branch,OS,webdir,statusDir,now,instance):
+    make_dir(os.path.join(webdir,OS))
+    make_dir(statusDir)
+
+    
+    # Create hidden file for node - for status reporting.
+    # print "Creating " + str(os.path.join(statusDir,OS + "." + branch + ".result"))
+    with open(os.path.join(statusDir,OS + "." + branch + ".result"), 'w') as content_file:
+        content_file.write("-1") 
+
+    # print  "Kickin tires"
+    # Kick the tires a bit, this helps the remote host to 'wake up', and for a network path to be learned by involved routers.
+    subprocess.call(timeout + " " + sshTime + " " + ssh + " -o ConnectTimeout=" + sshTimeout + " " + OS + ' "echo wakeup" > /dev/null 2>&1', shell=True)
+    subprocess.call(timeout + " " + sshTime + " " + ssh + " -o ConnectTimeout=" + sshTimeout + " " + OS + ' "echo get ready" > /dev/null 2>&1', shell=True)
+
+    # print "Scp script to remote box"
+    # Scp a script onto the remote box that we will later call.
+    subprocess.call(timeout + " " + sshTime + " " + scp + " -o ConnectTimeout=" + sshTimeout + " " + os.path.join(cwd,'installBranch.sh') + " " + OS + ":/root/installBranch.sh", shell=True)
+
+    # Get starting time
+    d1 = datetime.datetime.now()
+
+
+    # print "Starting installer"
+    # Start the fog installer.
+    subprocess.call(timeout + " " + fogTimeout + " " + ssh + " -o ConnectTimeout=" + sshTimeout + " " + OS + ' "/root/./installBranch.sh ' + branch + '"', shell=True)
+
+    # Get ending time.
+    d2 = datetime.datetime.now()
+
+    # Calculate duration.
+    duration = d2 - d1
+    duration = str(datetime.timedelta(seconds=duration.total_seconds()))
+    # Write duration to file.
+    with open(os.path.join(statusDir,OS + "." + branch + ".duration"), 'w') as content_file:
+        content_file.write(duration)
+
+
+    # print "Getting result file"
+    # Get the result file.
+    subprocess.call(timeout + " " + sshTime + " " + scp + " -o ConnectTimeout=" + sshTimeout + " " + OS + ":/root/result " + os.path.join(statusDir,OS + "." + branch + ".result"), shell=True)
+    # This should send the result code of the attempt to something like /tmp/debian9.master.result
+
+
+    # print "Getting output file"
+    # Get the output file.
+    subprocess.call(timeout + " " + sshTime + " " + scp + " -o ConnectTimeout=" + sshTimeout + " " + OS + ":/root/output " + os.path.join(webdir,OS,now + "_output.log"), shell=True)
+
+
+    # print "Getting fog log file"
+    # Get the fog log.
+    subprocess.call(timeout + " " + sshTime + " " + scp + " -o ConnectTimeout=" + sshTimeout + " " + OS + ":/root/git/fogproject/bin/error_logs/fog_error* " + os.path.join(webdir,OS,now + "_install.log"), shell=True)
+
+    # print "Getting apache logs"
+    # Get the apache error logs. Can be in only two places.
+    subprocess.call(timeout + " " + sshTime + " " + scp + " -o ConnectTimeout=" + sshTimeout + " " + OS + ":/var/log/httpd/error_log " + os.path.join(webdir,OS,now + "_apache.log") + " > /dev/null 2>&1", shell=True)
+    subprocess.call(timeout + " " + sshTime + " " + scp + " -o ConnectTimeout=" + sshTimeout + " " + OS + ":/var/log/apache2/error.log " + os.path.join(webdir,OS,now + "_apache.log") + " > /dev/null 2>&1", shell=True)
+
+    # print "Getting php-fpm logs"
+    # Get php-fpm logs. Can be in several places...
+    subprocess.call(timeout + " " + sshTime + " " + scp + " -o ConnectTimeout=" + sshTimeout + " " + OS + ":/var/log/php-fpm/www-error.log " + os.path.join(webdir,OS,now + "_php-fpm.log") + " > /dev/null 2>&1", shell=True)
+    subprocess.call(timeout + " " + sshTime + " " + scp + " -o ConnectTimeout=" + sshTimeout + " " + OS + ":/var/log/php-fpm/error.log " + os.path.join(webdir,OS,now + "_php-fpm.log") + " > /dev/null 2>&1", shell=True)
+    subprocess.call(timeout + " " + sshTime + " " + scp + " -o ConnectTimeout=" + sshTimeout + " " + OS + ":/var/log/php*-fpm.log " + os.path.join(webdir,OS,now + "_php-fpm.log") + " > /dev/null 2>&1", shell=True)
+
+    # print "Getting commit"
+    # Get the commit the remote node was using, just as a sainity check.
+    subprocess.call(timeout + " " + fogTimeout + " " + ssh + " -o ConnectTimeout=" + sshTimeout + " " + OS + ' "cd /root/git/fogproject;git rev-parse HEAD > /root/commit"', shell=True)
+    subprocess.call(timeout + " " + sshTime + " " + scp + " -o ConnectTimeout=" + sshTimeout + " " + OS + ":/root/commit " + os.path.join(statusDir,OS + "." + branch + ".commit"), shell=True)
+    # This should send just the commit that was used in the test to something like /tmp/debian9.master.commit
+
+    # print "Reading commit"
+    # Read the commit.
+    commit = read_file(os.path.join(statusDir,OS + "." + branch + ".commit"))
+
+    # Kill the instance.
+    instance.stop(Force=True)
+
+
+    # print "Rebuilding log"
+    # Rebuild the log file to have information at the top of it.
+    log = "Date=" + now + "\n"
+    log = log + "Branch=" + branch + "\n"
+    log = log + "Commit=" + commit # The commit comes back with a line feed in it.
+    log = log + "OS=" + OS + "\n"
+    log = log + "##### Begin Log #####\n"
+    log = log + read_file(os.path.join(webdir,OS,now + "_install.log"))
+
+    # print "Writing log"
+    # Write the new log.
+    with open(os.path.join(webdir,OS,now + "_install.log"), 'w') as content_file:
+        content_file.write(log)
+
+
+
+
 def add_ssh_identities():
     known_hosts_content = ""
     subprocess.call("echo '' > ~/.ssh/known_hosts", shell=True)
     for address in dnsAddresses:
         # Kick the tires a bit, this helps the remote host to 'wake up', and for a network path to be learned by involved routers.
+        subprocess.call(timeout + " " + sshTime + " " + ssh_keyscan + " -H " + address + " > /dev/null 2>&1", shell=True)
+        time.sleep(wait)
+        subprocess.call(timeout + " " + sshTime + " " + ssh_keyscan + " -H " + address + " > /dev/null 2>&1", shell=True)
+        time.sleep(wait)
+        subprocess.call(timeout + " " + sshTime + " " + ssh_keyscan + " -H " + address + " > /dev/null 2>&1", shell=True)
+        time.sleep(wait)
+        subprocess.call(timeout + " " + sshTime + " " + ssh_keyscan + " -H " + address + " > /dev/null 2>&1", shell=True)
+        time.sleep(wait)
         subprocess.call(timeout + " " + sshTime + " " + ssh_keyscan + " -H " + address + " > /dev/null 2>&1", shell=True)
         time.sleep(wait)
         subprocess.call(timeout + " " + sshTime + " " + ssh_keyscan + " -H " + address + " > /dev/null 2>&1", shell=True)
