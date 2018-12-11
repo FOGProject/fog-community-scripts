77bb3ee653b991b1131fd97d126f477a0f6850a2
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2018-12-10 19:22:42 -0600

    Move and organize

---
+++ fog-aws-testing/terraform/rhel7.tf
@@ -0,0 +1,57 @@
+
+resource "aws_instance" "rhel7" {
+  ami           = "${var.amis["rhel7"]}"
+  instance_type = "t3.micro"
+  subnet_id = "${aws_subnet.private-subnet.id}"
+  vpc_security_group_ids = ["${aws_security_group.allow-bastion.id}"]
+  associate_public_ip_address = false
+  key_name = "${aws_key_pair.ssh-key.key_name}"
+
+  root_block_device {
+    volume_type = "standard"
+    volume_size = 10
+    delete_on_termination = true
+  }
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key = "${file("/root/.ssh/fogtesting_private")}"
+    bastion_host = "${aws_instance.bastion.public_ip}"
+    bastion_user = "admin"
+    bastion_private_key = "${file("/root/.ssh/fogtesting_private")}"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo setenforce 0",
+      "sudo sed -i '/PermitRootLogin/d' /etc/ssh/sshd_config",
+      "echo 'PermitRootLogin prohibit-password' | sudo tee --append /etc/ssh/sshd_config",
+      "sudo mkdir -p /root/.ssh",
+      "sudo cp /home/ec2-user/.ssh/authorized_keys /root/.ssh/authorized_keys",
+      "sudo sed -i '/SELINUX=enforcing/d' /etc/selinux/config",
+      "echo 'SELINUX=permissive' | sudo tee --append /etc/selinux/config",
+      "sudo mkdir -p /root/git",
+      "sudo yum -y install git",
+      "sudo git clone https://github.com/FOGProject/fogproject /root/git/fogproject",
+      "sudo yum -y update",
+      "(sleep 10 && sudo reboot)&"
+    ]
+  }
+  tags {
+    Name = "${var.project}-rhel7"
+    Project = "${var.project}"
+    OS = "rhel7"
+  }
+  depends_on = ["aws_route_table_association.private-route-table-association"]
+}
+resource "aws_route53_record" "rhel7-dns-record" {
+  zone_id = "${aws_route53_zone.private-zone.zone_id}"
+  name    = "rhel7.fogtesting.cloud"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${aws_instance.rhel7.private_dns}"]
+}
+
+
+
+
---
+++ fog-aws-testing/terraform/templates.tf
@@ -0,0 +1,12 @@
+data "template_file" "ssh-config" {
+  template = "${file("templates/ssh-config.tpl")}"
+}
+
+
+data "template_file" "aws-config" {
+  template = "${file("templates/aws-config.tpl")}"
+  vars {
+    region = "${var.region}"
+  }
+}
+
