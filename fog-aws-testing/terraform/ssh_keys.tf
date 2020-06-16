ff3f5a11889b534de1c1d95949a79b9e9da1a631
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2020-06-16 08:32:34 -0500

    Correction

--- fog-aws-testing/terraform/ssh_keys.tf
+++ fog-aws-testing/terraform/ssh_keys.tf
@@ -9,7 +9,7 @@ resource "aws_key_pair" "ssh-key" {
 # We specify the path to it because it is not included in the repository.
 # This file is uploaded to the bastion server, and is used to issue commands to all the test OSs.
 variable "private_key_path" {
-  default = "/root/.ssh/fogtesting_private"
+  default = "~/.ssh/fogtesting_private"
 }
 
 # This is your personal SSH public key.
