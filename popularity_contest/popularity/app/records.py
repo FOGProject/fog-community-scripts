a53978dcf69e31229149f2589452d0db94fbc8fe
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2020-09-05 23:40:52 -0500

    Add missing file

---
+++ popularity_contest/popularity/app/records.py
@@ -0,0 +1,29 @@
+from app.functions import *
+
+
+@app.route("/api/records",methods = ['POST'])
+def post_records():
+    """
+    Accept a JSON body with fog version, os name, and os version.
+    Store this to the database.
+    """
+    os_name = ""
+    os_version = ""
+    fog_version = ""
+
+    record = request.get_json()
+    if "os_name" in record.keys():
+        os_name = record["os_name"]
+    if "os_version" in record.keys():
+        os_version = record["os_keys"]
+    if "fog_version" in record.keys():
+        fog_version = record["fog_version"]
+
+    sql = "INSERT INTO popularity (os_name,os_version,fog_version) VALUES (" + escape(str(os_name)) + "," + escape(str(os_version)) + "," + escape(str(fog_version)) + ");"
+
+    query(theSql=sql)
+
+    return jsonify({"message":"record recorded"}),200
+
+
+
