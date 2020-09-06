2d9330be0c99d038467ca008c00d4b5ac9bd6d3f
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2020-09-05 23:51:27 -0500

    Fix getting values

--- popularity_contest/popularity/app/records.py
+++ popularity_contest/popularity/app/records.py
@@ -10,17 +10,11 @@ def post_records():
     if not request.is_json:
         return jsonify({"message":"Request is missing json"}),400
 
-    os_name = ""
-    os_version = ""
-    fog_version = ""
     record = request.get_json()
 
-    if "os_name" in record.keys():
-        os_name = record["os_name"]
-    if "os_version" in record.keys():
-        os_version = record["os_version"]
-    if "fog_version" in record.keys():
-        fog_version = record["fog_version"]
+    os_name = json.get("os_name", "")
+    os_version = json.get("os_version", "")
+    fog_version = json.get("fog_version", "")
 
     sql = "INSERT INTO popularity (os_name,os_version,fog_version) VALUES (" + escape(str(os_name)) + "," + escape(str(os_version)) + "," + escape(str(fog_version)) + ");"
 
