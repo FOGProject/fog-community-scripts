6086332e7ba0014568f8758fa4b97b315206b195
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2020-09-27 14:25:13 -0500

    Corrections

--- fog_analytics/analytics/setup.sh
+++ fog_analytics/analytics/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # This is written to work on Debian 10 minimal.
-
 apt-get update
 apt-get -y remove python
 apt-get -y install apache2 libapache2-mod-wsgi-py3 python3-pip mariadb-server mariadb-client default-libmysqlclient-dev python3-mysqldb
+
 
 ## If a settings file exists, back it up.
 if [[ -f /opt/analytics/settings.json ]]; then
@@ -12,9 +12,11 @@ if [[ -f /opt/analytics/settings.json ]]; then
     cp /opt/analytics/settings.json /home/settings.json
 fi
 
+
 cp -r ../analytics /opt
 cd /opt/analytics
 rm -rf .git
+
 
 ## If we have a backed up settings file, put it back.
 if [[ -f /home/settings.json ]]; then
@@ -22,24 +24,25 @@ if [[ -f /home/settings.json ]]; then
     mv /home/settings.json /opt/analytics/settings.json
 fi
 
+
 pip3 install virtualenv 
+
 
 virtualenv flask
 # Note: Currently using mysql, but wanting to evaluate the mariadb module.
 flask/bin/pip install flask mysql mariadb
 
 
-if [[ -f /etc/apache2/conf-enabled ]]; then
-    rm -f /etc/apache2/conf-enabled
+# Do not overwrite this file if it exists because Let's Encrypt makes changes to it that need to stay.
+if [[ ! -f /etc/apache2/conf-enabled/apache-flask.conf ]]; then
+    cp apache-flask.conf /etc/apache2/conf-enabled
 fi
 
-cp apache-flask.conf /etc/apache2/conf-enabled
-#chown -R www-data:www-data app
-#chown -R www-data:www-data wsgi.py
 
 systemctl enable mariadb
 systemctl restart mariadb
 systemctl restart apache2
+
 
 # If we already have a database called analytics, exit.
 string=$(mysql -u root -e 'show databases' | grep 'analytics')
@@ -47,8 +50,10 @@ if [[ $string == *"analytics"* ]]; then
     exit
 fi
 
+
 # Uncomment this to drop database & start over.
 #mysql -u root -e "drop database analytics"
+
 
 # Setup database.
 mysql -u root < db.sql
