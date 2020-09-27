9bb01e373745ea0fa405b8301ec0001fc18c1d3e
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2020-09-27 14:15:02 -0500

    Update test file

--- fog_analytics/analytics/test.sh
+++ fog_analytics/analytics/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X POST -H "Content-Type: application/json"  -d '{"fog_version":"12.34.56","os_name":"Debian","os_version":"10"}' http://fog-analytics-entry.theworkmans.us:/api/records
+curl -X POST -H "Content-Type: application/json"  -d '{"fog_version":"12.34.56","os_name":"Debian","os_version":"10"}' https://fog-analytics-entries.theworkmans.us:/api/records
