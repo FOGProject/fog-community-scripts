52d52ac410d7811e87e0dc18b0cb344c8e7ef92e
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2020-09-27 15:22:32 -0500

    Temp settings for testing

--- fog-aws-testing/scripts/settings.py
+++ fog-aws-testing/scripts/settings.py
@@ -3,7 +3,8 @@ OSs = ["debian10","centos7","centos8","rhel7","rhel8","fedora32","ubuntu18_04","
 dnsAddresses = ["debian10.fogtesting.cloud","centos7.fogtesting.cloud","centos8.fogtesting.cloud","rhel7.fogtesting.cloud","rhel8.fogtesting.cloud","fedora32.fogtesting.cloud","ubuntu18_04.fogtesting.cloud","ubuntu20_04.fogtesting.cloud"]
 
 # The list of branches to process.
-branches = ["master","dev-branch","working-1.6"]
+#branches = ["master","dev-branch","working-1.6"]
+branches = ["popularity"]
 
 # The region we operate in, dictated by terraform.
 theRegion = "us-east-1"
