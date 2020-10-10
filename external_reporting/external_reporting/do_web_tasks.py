ce9fe41043e467c849fa5dc324eb2c74ebff0579
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2020-10-10 03:50:53 +0000

    Saving progress

--- external_reporting/external_reporting/do_web_tasks.py
+++ external_reporting/external_reporting/do_web_tasks.py
@@ -5,6 +5,9 @@ import MySQLdb as mysql
 from boto3 import client
 import os
 from datetime import datetime
+from matplotlib import pyplot, figure
+import numpy as np
+from sys import exit
 
 
 def query(theSql=None,json=False,single=False,getID=False):
@@ -42,25 +45,59 @@ with open(settingsFilePath, 'r') as settings_file:
 # Connect to database.
 db = mysql.connect(host=settings['MYSQL_HOST'],user=settings['MYSQL_USER'],passwd=settings['MYSQL_PASSWORD'], db=settings['MYSQL_DB'], port=settings['MYSQL_PORT'])
 
+"""
+# Get IDs from last 7 days.
+select id from versions_out_there where creation_time >= DATE(NOW()) - INTERVAL 7 DAY;
 
-# Produce bar graphs.
-
-
-
-
-
-
+# Get unique OS names from last 7 days.
+select distinct os_name from versions_out_there where id in (select id from versions_out_there where creation_time >= DATE(NOW()) - INTERVAL 7 DAY);
 
 
+# Number of fog systems running in the last 7 days.
+select count(id) from versions_out_there where creation_time >= DATE(NOW()) - INTERVAL 7 DAY;
+
+
+# select specific os count in last 7 days.
+select count(os_name) from versions_out_there where os_name = 'ubuntu' and id in (select id from versions_out_there where creation_time >= DATE(NOW()) - INTERVAL 7 DAY);
+
+
+# Get counts of distinct fog versions from the last 7 days.
+select distinct fog_version, count(*) as count from versions_out_there where id in (select id from versions_out_there where creation_time >= DATE(NOW()) - INTERVAL 7 DAY) GROUP BY fog_version;
+
+"""
 
 # Create s3 client.
 s3_client = client('s3')
-
 
 # Get the date & time in a format useful for timestamps.
 now = datetime.now()
 format = "%Y-%m-%d_%H-%M-%S"
 formatted_time = now.strftime(format)
+
+
+
+# Build a graph showing the number of fog versions out there.
+sql = "select distinct fog_version, count(*) as count from versions_out_there where id in (select id from versions_out_there where creation_time >= DATE(NOW()) - INTERVAL 7 DAY) GROUP BY fog_version;"
+
+results = query(theSql=sql,json=True)
+keys = [i["fog_version"] for i in results]
+values = [i["count"] for i in results]
+
+y_pos = np.arange(len(keys))
+pyplot.bar(y_pos, values, align='center', alpha=0.5)
+pyplot.xticks(y_pos, keys)
+pyplot.xticks(rotation=-90)
+pyplot.ylabel('FOG Versions')
+pyplot.title('FOG Version Usage Counts in last 7 days')
+fig = pyplot.gcf()
+fig.set_size_inches(18.5, 10.5)
+fig.savefig('/tmp/test.png', dpi=100)
+
+# Upload graph to s3.
+s3_client.upload_file("/tmp/test.png", settings["s3_bucket_name"], "archive/" + formatted_time + "/test.png", ExtraArgs={'ContentType': "image/png"})
+s3_client.upload_file("/tmp/test.png", settings["s3_bucket_name"], "test.png", ExtraArgs={'ContentType': "image/png"})
+
+
 
 
 # Dump the database
@@ -72,9 +109,6 @@ os.system(dump_command)
 compress_command = "tar -czf /tmp/db.tar.gz -C /tmp db.sql > /dev/null 2>&1"
 os.system(compress_command)
 
-# Set s3 extra args for index.html
-index_args = {'ContentType': "text/html"}
-
 
 # Upload database to base and archived.
 s3_client.upload_file("/tmp/db.tar.gz", settings["s3_bucket_name"], "archive/" + formatted_time + "/db.tar.gz")
@@ -82,8 +116,8 @@ s3_client.upload_file("/tmp/db.tar.gz", settings["s3_bucket_name"], "db.tar.gz")
 
 
 # Upload the index.html file to base and archived..
-s3_client.upload_file("/opt/external_reporting/index.html", settings["s3_bucket_name"], "archive/" + formatted_time + "/index.html", ExtraArgs=index_args)
-s3_client.upload_file("/opt/external_reporting/index.html", settings["s3_bucket_name"], "index.html", ExtraArgs=index_args)
+s3_client.upload_file("/opt/external_reporting/index.html", settings["s3_bucket_name"], "archive/" + formatted_time + "/index.html", ExtraArgs={'ContentType': "text/html"})
+s3_client.upload_file("/opt/external_reporting/index.html", settings["s3_bucket_name"], "index.html", ExtraArgs={'ContentType': "text/html"})
 
 
 
