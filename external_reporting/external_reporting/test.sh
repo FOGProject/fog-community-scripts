057a6d89845b0780004a147a14fe3e86430dac2a
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2020-11-08 15:35:05 -0600

    Updates to add CloudFront for SSL in the results, and to allow for redirects from stats.fogproject.org

--- external_reporting/external_reporting/test.sh
+++ external_reporting/external_reporting/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X POST -H "Content-Type: application/json"  -d '{"fog_version":"testing","os_name":"testing","os_version":"testing"}' https://fog-external-reporting-entries.theworkmans.us:/api/records
+curl -X POST -H "Content-Type: application/json"  -d '{"fog_version":"testing","os_name":"testing","os_version":"testing"}' https://stats-api.fogproject.org/api/records
