8cf6bfb38f5d493187ba4602937e34cd53461fa7
Tom Elliott <mastacontrola@users.noreply.github.com>
2021-01-14 13:18:51 -0600

    Update copybacktrunk.sh

--- CopyBackTrunk/copybacktrunk.sh
+++ CopyBackTrunk/copybacktrunk.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 path=$1
+configpath=$2
 [[ -z $path || ! -e "$HOME/$path" ]] && path="$HOME/fogproject"
+[[ -z $configpath || ! -e "$configpath" ]] && configpath="$HOME/config.class.php"
+[[ ! -e "$configpath" && -e /var/www/fog/lib/fog/config.class.php ]] && cp /var/www/fog/lib/fog/config.class.php $configpath
+[[ ! -e $configpath ]] && {
+    echo "No configuration file available. Please make sure this file exists"
+    exit 1
+}
 rsync -a --no-links -heP --delete $path/packages/web/ /var/www/fog/
-cp ~/config.class.php /var/www/fog/lib/fog/config.class.php
+cp $configpath /var/www/fog/lib/fog/config.class.php
 chown -R apache:apache /var/www/fog
 chown -R fogproject:apache /var/www/fog/service/ipxe
