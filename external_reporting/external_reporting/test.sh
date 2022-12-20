3a53ac08975249095cb4d00dc97209e95a563f9d
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2022-12-19 22:56:53 -0600

    Removed files, Add note for new location

--- external_reporting/external_reporting/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -X POST -H "Content-Type: application/json"  -d '{"fog_version":"testing","os_name":"testing","os_version":"testing"}' https://fog-external-reporting-entries.theworkmans.us:/api/records
--- fog-aws-testing/scripts/stop_all_instances.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-
-from functions import *
-
-
-stop_instances()
-
-
